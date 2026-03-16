6e8bb31fdba335df530975acaf96fe777f47826b
Ihab <88992675+Waa-Ihaab@users.noreply.github.com>
2026-03-16 10:38:24 +0100

    Adding Buzzer&BDD

--- tempereaturedis.ino
+++ tempereaturedis.ino
@@ -4,53 +4,62 @@
 #define ONE_WIRE_BUS 2
 #define ECHO_PIN 4
 #define TRIG_PIN 3
+#define BUZZER_PIN 8
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  digitalWrite(BUZZER_PIN, LOW);
+
   sensors.begin();
 }
 
 float readDistanceCM() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
+
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
+
   digitalWrite(TRIG_PIN, LOW);
-  int duration = pulseIn(ECHO_PIN, HIGH);
-  return duration * 0.034 / 2;
+
+  long duration = pulseIn(ECHO_PIN, HIGH, 30000);
+
+  if (duration == 0) {
+    return 999;
+  }
+
+  return duration * 0.034 / 2.0;
 }
-
-
-int i = 0;
-
-
 
 void loop() {
   float distance = readDistanceCM();
+  bool isNearby = distance < 5.0;
 
-  bool isNearby = distance < 20;
   digitalWrite(LED_BUILTIN, isNearby);
 
-  Serial.print("Measured distance: ");
-  Serial.println(readDistanceCM());
-  delay(100);
+  if (isNearby) {
+    tone(BUZZER_PIN, 1000);
+  } else {
+    noTone(BUZZER_PIN);
+    digitalWrite(BUZZER_PIN, LOW);
+  }
 
-
-  i++;
   sensors.requestTemperatures();
   float tempC = sensors.getTempCByIndex(0);
-  Serial.print(i);
-  Serial.print(")=>");
-  Serial.print("Temperature: ");
+
+  // affichage simple
   Serial.print(tempC);
-  Serial.println(" °C");
-  delay(1000);
+  Serial.print(",");
+  Serial.println(distance);
 
-
+  delay(300);
 }
