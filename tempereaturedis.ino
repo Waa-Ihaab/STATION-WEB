7e413a75ae0f107174db89f0e1e3d2021f253ad7
Ihab <88992675+Waa-Ihaab@users.noreply.github.com>
2026-03-16 22:34:37 +0100

    Rx>Tx,Tx>Rx

--- tempereaturedis.ino
+++ tempereaturedis.ino
@@ -1,3 +1,4 @@
+#include <SoftwareSerial.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
@@ -6,11 +7,23 @@
 #define TRIG_PIN 3
 #define BUZZER_PIN 8
 
+#define BT_RX 7
+#define BT_TX 6
+
+int buzzerState = 0;
+
+SoftwareSerial BT(BT_RX, BT_TX);
+
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+bool lastBuzzerState = false;
+
+
+
 
 void setup() {
   Serial.begin(9600);
+  BT.begin(9600);
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(TRIG_PIN, OUTPUT);
@@ -41,25 +54,51 @@ float readDistanceCM() {
 }
 
 void loop() {
+
   float distance = readDistanceCM();
   bool isNearby = distance < 5.0;
 
   digitalWrite(LED_BUILTIN, isNearby);
 
+  int buzzerTrigger = 0;
+
   if (isNearby) {
-    tone(BUZZER_PIN, 1000);
-  } else {
+
+    tone(BUZZER_PIN, 1500);
+    delay(80);
+    tone(BUZZER_PIN, 500);
+    delay(80);
+
+    // Détection OFF → ON
+    if (!lastBuzzerState) {
+      buzzerTrigger = 1;
+    }
+
+    lastBuzzerState = true;
+
+  } 
+  else {
+
     noTone(BUZZER_PIN);
     digitalWrite(BUZZER_PIN, LOW);
+
+    lastBuzzerState = false;
   }
 
   sensors.requestTemperatures();
   float tempC = sensors.getTempCByIndex(0);
 
-  // affichage simple
   Serial.print(tempC);
   Serial.print(",");
-  Serial.println(distance);
+  Serial.print(distance);
+  Serial.print(",");
+  Serial.println(buzzerTrigger);
+
+  BT.print(tempC);
+  BT.print(",");
+  BT.print(distance);
+  BT.print(",");
+  BT.println(buzzerTrigger);
 
   delay(300);
 }
